99b58d35ea6a7c9f5b7dc9563d43a3cec33da4f5
TheLastScrub <67403321+TheLastScrub@users.noreply.github.com>
2023-01-21 18:30:15 -0800

    layout improvements

--- module/item/item-sheet.js
+++ module/item/item-sheet.js
@@ -3,24 +3,53 @@
  * @extends {ItemSheet}
  */
 export class TroikaItemSheet extends ItemSheet {
-    /** @override */
-    static get defaultOptions() {
-        return mergeObject(super.defaultOptions, {
-            classes: ["troika", "sheet", "item"],
-            width: 520,
-            height: 480,
-            tabs: [{ navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "attributes" }]
+  /** @override */
+  static get defaultOptions() {
+    return mergeObject(super.defaultOptions, {
+        classes: ["troika", "sheet", "item"],
+        width: 520,
+        height: 480,
+        tabs: [{ navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "attributes" }]
     });
   }
 
-    /** @override */
-    async getData() {
-        const data = super.getData();
-        data.enrichedDescription = await TextEditor.enrichHTML(this.object.system.description, {async: true});
-        return data;
-    }
+  /** @override */
+  async getData() {
+    const data = super.getData();
+    data.enrichedDescription = await TextEditor.enrichHTML(this.object.system.description, {async: true});
 
-     /** @override */
+    this._prepareItemData(data);
+
+    return data;
+  }
+
+  _prepareItemData(data){
+    
+    const item = data.item;
+
+    item.system.displayName = item.name;
+    item.system.tooltip = "";
+
+    if(item.type === 'gear'){
+      
+      if(item.system.canAttack === true){
+        if(item.system.requiresTwoHands === true){
+          item.system.displayName += "*";
+          item.system.tooltip += "Requires two hands to use";
+        }
+        if(item.system.armourIgnored > 0){
+          item.system.displayName += "#";
+          if(item.tooltip !== ""){
+            item.system.tooltip += ", ";
+          }
+          item.system.tooltip += 'Item ignores ${item.system.armourIgnored} points of armour';
+        }
+      }
+    }
+    console.log(data);
+  }
+
+  /** @override */
   get template() {
     const path = "systems/troika/templates/item";
     
@@ -31,6 +60,6 @@ export class TroikaItemSheet extends ItemSheet {
     else{
         return `${path}/item-${this.item.type}-sheet.html`;
     }
-    
   }
+
 }
