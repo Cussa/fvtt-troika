34c05d54b2be091f6b655d262fe385ba066e0480
Cussa Mitre <cussa.mitre@gmail.com>
2024-07-06 00:44:56 +0100

    ✨ Show all combatants
    Fix #17

--- module/combat/combat-tracker.mjs
+++ module/combat/combat-tracker.mjs
@@ -21,4 +21,31 @@ export class TroikaCombatTracker extends CombatTracker {
       context.turns = context.turns.slice(0, context.turn + 1);
     return context;
   }
+
+  /** @override */
+  activateListeners(html) {
+    super.activateListeners(html);
+    html.find(".combat-show-all").click(ev => this._showAllCombatants(ev));
+  }
+
+  async _showAllCombatants(event) {
+    event.preventDefault();
+    const tokenIds = game.combat.getFlag("troika", "tokenIds");
+    let tokenInfo = [];
+
+    for (const tokenId of tokenIds) {
+      const token = game.scenes.current.tokens.get(tokenId);
+      if (!token)
+        continue;
+
+      tokenInfo.push(token);
+      console.log(token);
+    }
+    const content = await renderTemplate("systems/troika/templates/combat/show-all-combatants.hbs", { tokens: tokenInfo });
+
+    Dialog.prompt({
+      title: "Combatants",
+      content
+    })
+  }
 }
